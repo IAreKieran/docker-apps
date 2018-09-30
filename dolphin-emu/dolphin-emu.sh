797e099cf87a87c0a1c65f3e5349d03b8e39d68b
dypher <i_are_kieran@hotmail.co.uk>
2018-09-30 20:51:14 +0100

    backup

--- dolphin-emu/dolphin-emu.sh
+++ dolphin-emu/dolphin-emu.sh
@@ -7,5 +7,5 @@ docker build -t docker-apps:dolphin-emu "$PROJECT_DIR/dolphin-emu"
 #docker run --rm -d --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/kieran/Desktop/games:/home/dolphin-emu/games -e DISPLAY=unix$DISPLAY --name=dolphin-emu docker-apps:dolphin-emu
 #docker cp dolphin-emu:/home/dolphin-emu/.local/share/dolphin-emu /home/kieran/projects/docker-apps/dolphin-emu/data
 
-docker run --rm -d --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/kieran/Desktop/games:/home/dolphin-emu/games -v /home/kieran/projects/docker-apps/dolphin-emu/data:/home/dolphin-emu/.local/share/dolphin-emu -e DISPLAY=unix$DISPLAY --name=dolphin-emu docker-apps:dolphin-emu
+docker run --rm -d --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/kieran/Desktop/games:/home/dolphin-emu/games -v /home/kieran/projects/docker-apps/dolphin-emu/data:/home/dolphin-emu/.local/share -e DISPLAY=unix$DISPLAY --name=dolphin-emu docker-apps:dolphin-emu
 
