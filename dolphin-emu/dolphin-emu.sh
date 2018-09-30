c67c1bf04bed5e014d4a28626b98b2bf8051a093
dypher <i_are_kieran@hotmail.co.uk>
2018-09-30 20:32:36 +0100

    initial dolphin files

--- dolphin-emu/dolphin-emu.sh
+++ dolphin-emu/dolphin-emu.sh
@@ -3,4 +3,9 @@ PROJECT_DIR=`git rev-parse --show-toplevel`
 
 docker build -t docker-apps:dolphin-emu "$PROJECT_DIR/dolphin-emu"
 
-docker run --rm -d --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --name=dolphin-emu docker-apps:dolphin-emu
+# To get basic files for mount directo
+#docker run --rm -d --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/kieran/Desktop/games:/home/dolphin-emu/games -e DISPLAY=unix$DISPLAY --name=dolphin-emu docker-apps:dolphin-emu
+#docker cp dolphin-emu:/home/dolphin-emu/.local/share/dolphin-emu /home/kieran/projects/docker-apps/dolphin-emu/data
+
+docker run --rm -d --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/kieran/Desktop/games:/home/dolphin-emu/games -v /home/kieran/projects/docker-apps/dolphin-emu/data:/home/dolphin-emu/.local/share/dolphin-emu -e DISPLAY=unix$DISPLAY --name=dolphin-emu docker-apps:dolphin-emu
+
