fda96cdf47ce23fec70fc9cefffff6c714faaa87
dypher <i_are_kieran@hotmail.co.uk>
2018-09-30 23:11:56 +0100

    tidied

--- dolphin-emu/dolphin-emu.sh
+++ dolphin-emu/dolphin-emu.sh
@@ -8,12 +8,23 @@ data_dir="$PROJECT_DIR/dolphin-emu/data"
 if [ ! -d "$data_dir" ]; then
   mkdir /home/kieran/projects/docker-apps/dolphin-emu/data
 
-  docker run --rm -d --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/kieran/Desktop/games:/home/dolphin-emu/games -e DISPLAY=unix$DISPLAY --name=dolphin-emu docker-apps:dolphin-emu
+  docker run --rm -d \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=unix$DISPLAY \
+    --name=dolphin-emu docker-apps:dolphin-emu
 
   docker cp dolphin-emu:/home/dolphin-emu/.local/share/dolphin-emu /home/kieran/projects/docker-apps/dolphin-emu/data/.local
   docker cp dolphin-emu:/home/dolphin-emu/.config/dolphin-emu /home/kieran/projects/docker-apps/dolphin-emu/data/.config
   docker kill dolphin-emu
 fi
 
-docker run --rm -d --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/kieran/Desktop/games:/home/dolphin-emu/games -v /home/kieran/projects/docker-apps/dolphin-emu/data/.local:/home/dolphin-emu/.local/share/dolphin-emu -v /home/kieran/projects/docker-apps/dolphin-emu/data/.config:/home/dolphin-emu/.config/dolphin-emu -e DISPLAY=unix$DISPLAY --name=dolphin-emu docker-apps:dolphin-emu
+docker run --rm -d \
+  --net=host \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /home/kieran/Desktop/games:/home/dolphin-emu/games \
+  -v /home/kieran/projects/docker-apps/dolphin-emu/data/.local:/home/dolphin-emu/.local/share/dolphin-emu \
+  -v /home/kieran/projects/docker-apps/dolphin-emu/data/.config:/home/dolphin-emu/.config/dolphin-emu \
+  -e DISPLAY=unix$DISPLAY \
+  --name=dolphin-emu \
+  docker-apps:dolphin-emu
 
