8a4e30e2dd336c3a78975be82c849bd2ffb4a2e1
dypher <i_are_kieran@hotmail.co.uk>
2018-09-30 19:19:45 +0100

    removed reliance on separate git repo

--- dolphin-emu/dolphin-emu.sh
+++ dolphin-emu/dolphin-emu.sh
@@ -1,3 +1,6 @@
-docker build -t docker-apps:dolphin-emu /home/kieran/projects/third_party/docker-dolphin/vanilla
+PROJECT_DIR=`git rev-parse --show-toplevel`
+
+
+docker build -t docker-apps:dolphin-emu "$PROJECT_DIR/dolphin-emu"
 
 docker run --rm -d --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --name=dolphin-emu docker-apps:dolphin-emu
