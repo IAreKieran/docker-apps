e50d2ec9721a7d50e266380873ee4d8ce143829d
dypher <i_are_kieran@hotmail.co.uk>
2018-10-01 20:53:14 +0100

    tidied up & generalised dolphin-emu

--- dolphin-emu/dolphin-emu.sh
+++ dolphin-emu/dolphin-emu.sh
@@ -1,16 +1,30 @@
+# User set variables
+HOST_GAMES_DIR="/home/kieran/Desktop/games"
+
+# docker-apps settings
 PROJECT_DIR=`git rev-parse --show-toplevel`
 DOLPHIN_EMU_DIR="$PROJECT_DIR/dolphin-emu"
 
 HOST_USER_ID=`id -u`
 HOST_USER_GID=`id -g`
 
+# Mount points - Host
+DOLPHIN_EMU_DATA="$DOLPHIN_EMU_DIR/data"
+HOST_DATA_1="$DOLPHIN_EMU_DATA/.local"
+HOST_DATA_2="$DOLPHIN_EMU_DATA/.config"
+
+# Mount points - Container
+CONTAINER_GAMES_DIR="/home/docker-apps/games"
+CONTAINER_DATA_1="/home/docker-apps/.local/share/dolphin-emu"
+CONTAINER_DATA_2="/home/docker-apps/.config/dolphin-emu"
+
+# Build
 docker build \
   --build-arg "HOST_USER_ID=$HOST_USER_ID" \
   --build-arg "HOST_USER_GID=$HOST_USER_GID" \
   -t docker-apps:dolphin-emu $DOLPHIN_EMU_DIR
 
-# To get basic files created on install for mount directories
-DOLPHIN_EMU_DATA="$DOLPHIN_EMU_DIR/data"
+# If it's the first run, copy over initial user data files to host to be used as mounts
 if [ ! -d "$DOLPHIN_EMU_DATA" ]; then
   mkdir $DOLPHIN_EMU_DATA
 
@@ -19,10 +33,11 @@ if [ ! -d "$DOLPHIN_EMU_DATA" ]; then
     -e DISPLAY=unix$DISPLAY \
     --name=dolphin-emu docker-apps:dolphin-emu
 
-  wait_cmd='while [ ! -d "/home/dolphin-emu/.local/share/dolphin-emu" ]; do echo "directory not ready"; sleep 1; done'
-  docker exec dolphin-emu bash -c "$wait_cmd" # Wait for dolphin to finish making files on first startup
-  docker cp dolphin-emu:/home/dolphin-emu/.local/share/dolphin-emu "$DOLPHIN_EMU_DATA/.local"
-  docker cp dolphin-emu:/home/dolphin-emu/.config/dolphin-emu "$DOLPHIN_EMU_DATA/.config"
+  # Wait for dolphin to finish making files created on first startup, then copy to host
+  wait_cmd="while [ ! -d $CONTAINER_DATA_1 ] || [ ! -d $CONTAINER_DATA_2 ]; do sleep 0.2; done"
+  docker exec dolphin-emu bash -c "$wait_cmd"
+  docker cp dolphin-emu:$CONTAINER_DATA_1 $HOST_DATA_1
+  docker cp dolphin-emu:$CONTAINER_DATA_2 $HOST_DATA_2
   docker kill dolphin-emu
   docker wait dolphin-emu
 fi
@@ -30,9 +45,9 @@ fi
 docker run --rm -d \
   --hostname=dolphin-emu \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v /home/kieran/Desktop/games:/home/dolphin-emu/games \
-  -v "$DOLPHIN_EMU_DATA/.local:/home/dolphin-emu/.local/share/dolphin-emu" \
-  -v "$DOLPHIN_EMU_DATA/.config:/home/dolphin-emu/.config/dolphin-emu" \
+  -v "$HOST_GAMES_DIR:$CONTAINER_GAMES_DIR" \
+  -v "$HOST_DATA_1:$CONTAINER_DATA_1" \
+  -v "$HOST_DATA_2:$CONTAINER_DATA_2" \
   -e DISPLAY=unix$DISPLAY \
   --name=dolphin-emu \
   docker-apps:dolphin-emu
