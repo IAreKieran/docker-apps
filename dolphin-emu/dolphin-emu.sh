2ad4995a52791669822c487daf6ea603a265ec3a
dypher <i_are_kieran@hotmail.co.uk>
2018-10-01 16:47:08 +0100

    less hackish way to wait for dolphin to init files on startup

--- dolphin-emu/dolphin-emu.sh
+++ dolphin-emu/dolphin-emu.sh
@@ -19,14 +19,16 @@ if [ ! -d "$DOLPHIN_EMU_DATA" ]; then
     -e DISPLAY=unix$DISPLAY \
     --name=dolphin-emu docker-apps:dolphin-emu
 
-  sleep 3 # Need a better way to wait for dolphin to initialise. .local is last
+  wait_cmd='while [ ! -d "/home/dolphin-emu/.local/share/dolphin-emu" ]; do echo "directory not ready"; sleep 1; done'
+  docker exec dolphin-emu bash -c "$wait_cmd" # Wait for dolphin to finish making files on first startup
   docker cp dolphin-emu:/home/dolphin-emu/.local/share/dolphin-emu "$DOLPHIN_EMU_DATA/.local"
   docker cp dolphin-emu:/home/dolphin-emu/.config/dolphin-emu "$DOLPHIN_EMU_DATA/.config"
   docker kill dolphin-emu
+  docker wait dolphin-emu
 fi
 
 docker run --rm -d \
-  --net=host \
+  --hostname=dolphin-emu \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /home/kieran/Desktop/games:/home/dolphin-emu/games \
   -v "$DOLPHIN_EMU_DATA/.local:/home/dolphin-emu/.local/share/dolphin-emu" \
