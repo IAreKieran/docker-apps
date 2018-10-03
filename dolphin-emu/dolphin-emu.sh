7b4959640e8ada1a311a7ece48eebaf3aef25815
dypher <i_are_kieran@hotmail.co.uk>
2018-10-03 11:27:31 +0100

    generalised sh scripts a bit

--- dolphin-emu/dolphin-emu.sh
+++ dolphin-emu/dolphin-emu.sh
@@ -1,17 +1,20 @@
 # User set variables
 HOST_GAMES_DIR="/home/kieran/Desktop/games"
 
+# App name
+APP='dolphin-emu'
+
 # docker-apps settings
 PROJECT_DIR=`git rev-parse --show-toplevel`
-DOLPHIN_EMU_DIR="$PROJECT_DIR/dolphin-emu"
+APP_DIR="$PROJECT_DIR/$APP"
+APP_DATA="$APP_DIR/data"
 
 HOST_USER_ID=`id -u`
 HOST_USER_GID=`id -g`
 
 # Mount points - Host
-DOLPHIN_EMU_DATA="$DOLPHIN_EMU_DIR/data"
-HOST_DATA_1="$DOLPHIN_EMU_DATA/.local"
-HOST_DATA_2="$DOLPHIN_EMU_DATA/.config"
+HOST_DATA_1="$APP_DATA/.local"
+HOST_DATA_2="$APP_DATA/.config"
 
 # Mount points - Container
 CONTAINER_GAMES_DIR="/home/docker-apps/games"
@@ -22,33 +25,33 @@ CONTAINER_DATA_2="/home/docker-apps/.config/dolphin-emu"
 docker build \
   --build-arg "HOST_USER_ID=$HOST_USER_ID" \
   --build-arg "HOST_USER_GID=$HOST_USER_GID" \
-  -t docker-apps:dolphin-emu $DOLPHIN_EMU_DIR
+  -t docker-apps:$APP $APP_DIR
 
 # If it's the first run, copy over initial user data files to host to be used as mounts
-if [ ! -d "$DOLPHIN_EMU_DATA" ]; then
-  mkdir $DOLPHIN_EMU_DATA
+if [ ! -d "$APP_DATA" ]; then
+  mkdir $APP_DATA
 
   docker run --rm -d \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
-    --name=dolphin-emu docker-apps:dolphin-emu
+    --name=$APP docker-apps:$APP
 
   # Wait for dolphin to finish making files created on first startup, then copy to host
   wait_cmd="while [ ! -d $CONTAINER_DATA_1 ] || [ ! -d $CONTAINER_DATA_2 ]; do sleep 0.2; done"
-  docker exec dolphin-emu bash -c "$wait_cmd"
-  docker cp dolphin-emu:$CONTAINER_DATA_1 $HOST_DATA_1
-  docker cp dolphin-emu:$CONTAINER_DATA_2 $HOST_DATA_2
-  docker kill dolphin-emu
-  docker wait dolphin-emu
+  docker exec $APP bash -c "$wait_cmd"
+  docker cp $APP:$CONTAINER_DATA_1 $HOST_DATA_1
+  docker cp $APP:$CONTAINER_DATA_2 $HOST_DATA_2
+  docker kill $APP
+  docker wait $APP
 fi
 
 docker run --rm -d \
-  --hostname=dolphin-emu \
+  --hostname=$APP \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v "$HOST_GAMES_DIR:$CONTAINER_GAMES_DIR" \
   -v "$HOST_DATA_1:$CONTAINER_DATA_1" \
   -v "$HOST_DATA_2:$CONTAINER_DATA_2" \
   -e DISPLAY=unix$DISPLAY \
-  --name=dolphin-emu \
-  docker-apps:dolphin-emu
+  --name=$APP \
+  docker-apps:$APP
 
--- firefox/firefox.sh
+++ firefox/firefox.sh
@@ -1,5 +1,7 @@
+APP='firefox'
+
 PROJECT_DIR=`git rev-parse --show-toplevel`
-GUI_DIR="$PROJECT_DIR/GUI"
+APP_DIR="$PROJECT_DIR/$APP"
 
 HOST_USER_ID=`id -u`
 HOST_USER_GID=`id -g`
@@ -7,13 +9,12 @@ HOST_USER_GID=`id -g`
 docker build \
   --build-arg "HOST_USER_ID=$HOST_USER_ID" \
   --build-arg "HOST_USER_GID=$HOST_USER_GID" \
-  -t docker-apps:GUI $GUI_DIR
+  -t docker-apps:$APP $APP_DIR
 
 
 docker run --rm -d \
-  --hostname=GUI \
+  --hostname=$APP \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -e DISPLAY=unix$DISPLAY \
-  --name=GUI \
-  docker-apps:GUI
-
+  --name=$APP \
+  docker-apps:$APP
