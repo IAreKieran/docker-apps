f746a768fc71ee97a65a63052503981e18e026ac
dypher <i_are_kieran@hotmail.co.uk>
2018-10-06 13:55:32 +0100

    dolphin-emu: added support for mayflash adapter, sound & GPU

--- dolphin-emu/dolphin-emu.sh
+++ dolphin-emu/dolphin-emu.sh
@@ -18,6 +18,16 @@ CONTAINER_GAMES_DIR="/home/docker-apps/games"
 CONTAINER_DATA_1="/home/docker-apps/.local/share/dolphin-emu"
 CONTAINER_DATA_2="/home/docker-apps/.config/dolphin-emu"
 
+# Devices
+HOST_INPUT_1="/sys/bus/usb/devices"
+# For mayflash adapater, make more specific later
+# Note that /sys/devices also works, but anecdotally seems slightly slower
+HOST_INPUT_2="/dev/snd"   # Sound
+HOST_INPUT_3="/dev/dri"   # So dolphin can take advantage of GPU
+CONTAINER_INPUT_1="$HOST_INPUT_1:ro" # Make read-only option less hacky later
+CONTAINER_INPUT_2=$HOST_INPUT_2
+CONTAINER_INPUT_3=$HOST_INPUT_3
+
 # Build
 docker build \
   --build-arg "HOST_USER_ID=$HOST_USER_ID" \
@@ -31,7 +41,8 @@ if [ ! -d "$DOLPHIN_EMU_DATA" ]; then
   docker run --rm -d \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
-    --name=dolphin-emu docker-apps:dolphin-emu
+    --name=dolphin-emu \
+    docker-apps:dolphin-emu
 
   # Wait for dolphin to finish making files created on first startup, then copy to host
   wait_cmd="while [ ! -d $CONTAINER_DATA_1 ] || [ ! -d $CONTAINER_DATA_2 ]; do sleep 0.2; done"
@@ -42,13 +53,16 @@ if [ ! -d "$DOLPHIN_EMU_DATA" ]; then
   docker wait dolphin-emu
 fi
 
-docker run --rm -d \
-  --hostname=dolphin-emu \
+# Set up display, games, data, devices, misc
+docker run --rm -d --privileged \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -e DISPLAY=unix$DISPLAY \
   -v "$HOST_GAMES_DIR:$CONTAINER_GAMES_DIR" \
   -v "$HOST_DATA_1:$CONTAINER_DATA_1" \
   -v "$HOST_DATA_2:$CONTAINER_DATA_2" \
-  -e DISPLAY=unix$DISPLAY \
+  -v "$HOST_INPUT_1:$CONTAINER_INPUT_1" \
+  -v "$HOST_INPUT_2:$CONTAINER_INPUT_2" \
+  -v "$HOST_INPUT_3:$CONTAINER_INPUT_3" \
   --name=dolphin-emu \
+  --hostname=dolphin-emu \
   docker-apps:dolphin-emu
-
